299514b8112184424b153b1c570d288b142b85eb
Ealden Esto E. Escañan <ealden@escanan.com>
2018-06-05 12:16:37 +0900

    Collapsed it to inline

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -2,26 +2,18 @@ require_relative 'fizzbuzz'
 
 describe 'FizzBuzz' do
   context 'with input divisible by 3' do
-    it "should return 'fizz'" do
-      expect(fizzbuzz(3)).to eq 'fizz'
-    end
+    it { expect(fizzbuzz(3)).to eq 'fizz' }
   end
 
   context 'with input divisible by 5' do
-    it "should return 'buzz'" do
-      expect(fizzbuzz(5)).to eq 'buzz'
-    end
+    it { expect(fizzbuzz(5)).to eq 'buzz' }
   end
 
   context 'with input divisible by 3 and 5' do
-    it "should return 'fizzbuzz'" do
-      expect(fizzbuzz(15)).to eq 'fizzbuzz'
-    end
+    it { expect(fizzbuzz(15)).to eq 'fizzbuzz' }
   end
 
   context 'with input not divisible by 3 or 5' do
-    it 'should return the input' do
-      expect(fizzbuzz(1)).to eq 1
-    end
+    it { expect(fizzbuzz(1)).to eq 1 }
   end
 end
